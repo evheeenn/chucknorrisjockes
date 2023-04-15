ace5d793cabeb50f03a9d26a354c5b011c8487d6
evheeenn <nermalord@gmail.com>
2023-04-15 13:32:31 +0300

    Baf fixing commit

--- script.js
+++ script.js
@@ -1,267 +1,271 @@
-let body = document.querySelector('body')
-let mainWrapper = document.querySelector('#wrapper')
-let wrapper = document.querySelector('#jokes-block')
-let fromCategories = document.querySelector('input[value="fromcategories"]')
-let randomJoke = document.querySelector('#random')
-let menuButton = document.querySelector('#menu-button')
-let filter = document.querySelector('#filter')
-let menuImg = document.querySelector('#menu-img')
-let favJokes = document.querySelector('#favourite-jokes-section')
-let categoriesListField = document.querySelector('#categories-list')
-let clickCount = 0
+let body = document.querySelector("body");
+let mainWrapper = document.querySelector("#wrapper");
+let wrapper = document.querySelector("#jokes-block");
+let fromCategories = document.querySelector('input[value="fromcategories"]');
+let randomJoke = document.querySelector("#random");
+let menuButton = document.querySelector("#menu-button");
+let filter = document.querySelector("#filter");
+let menuImg = document.querySelector("#menu-img");
+let favJokes = document.querySelector("#favourite-jokes-section");
+let categoriesListField = document.querySelector("#categories-list");
+let clickCount = 0;
 
-favJokes.style.height = mainWrapper.style.height
+favJokes.style.height = mainWrapper.style.height;
 
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener("click", () => {
+  clickCount++;
+  menuImg.src = "visual/menu-close.svg";
+  favJokes.style.right = "0";
+  filter.style.opacity = "1";
+  filter.style.display = "block";
 
-	clickCount++
-	menuImg.src = 'visual/menu-close.svg'
-	favJokes.style.right = '0'
-	filter.style.opacity = '1'
-	filter.style.display = 'block'
-	
-	if(clickCount % 2 === 0){
-		menuImg.src = 'visual/menu-open.svg'
-		favJokes.style.right = '-100%'
-		filter.style.opacity = '0'
-	    filter.style.display = 'none'
-		
-	}
-})
+  if (clickCount % 2 === 0) {
+    menuImg.src = "visual/menu-open.svg";
+    favJokes.style.right = "-100%";
+    filter.style.opacity = "0";
+    filter.style.display = "none";
+  }
+});
 
-randomJoke.addEventListener('change', () => {
-		input.style.display = 'none'
-})
+randomJoke.addEventListener("change", () => {
+  input.style.display = "none";
+});
 
-let searchCheckbox = document.querySelector('input[value="search"]')
+let searchCheckbox = document.querySelector('input[value="search"]');
 
-searchCheckbox.addEventListener('change', () => {
-		input.style.display = 'block'
-})
+searchCheckbox.addEventListener("change", () => {
+  input.style.display = "block";
+});
 
 let createCategories = (res) => {
-	
-	res.forEach((el) => {
-		let label = document.createElement('label')
-		label.textContent = el.toUpperCase()
-		label.setAttribute('for', `${el}`)
+  res.forEach((el) => {
+    let label = document.createElement("label");
+    label.textContent = el.toUpperCase();
+    label.setAttribute("for", `${el}`);
 
-		let radio = document.createElement('input')
-		radio.setAttribute('name', 'radio')
-		radio.setAttribute('type', 'radio')
-		radio.setAttribute('value', el)
-		radio.setAttribute('id', el)
-		radio.classList.add('categories')
+    let radio = document.createElement("input");
+    radio.setAttribute("name", "radio");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("value", el);
+    radio.setAttribute("id", el);
+    radio.classList.add("categories");
 
-		label.classList.add('label-categories')
-		radio.classList.add('radio-categories')
-		
-		
-		categoriesListField.appendChild(radio)
-		categoriesListField.appendChild(label)
-		
-	})
-	console.log(res)
-}
+    label.classList.add("label-categories");
+    radio.classList.add("radio-categories");
+
+    categoriesListField.appendChild(radio);
+    categoriesListField.appendChild(label);
+  });
+  console.log(res);
+};
 
 let getCategories = () => {
-	fetch('https://api.chucknorris.io/jokes/categories')
-	.then(res => res.json())
-	.then(res => createCategories(res))
-}
+  fetch("https://api.chucknorris.io/jokes/categories")
+    .then((res) => res.json())
+    .then((res) => createCategories(res));
+};
 
-getCategories()
+getCategories();
 
-fromCategories.addEventListener('change', () => {
+fromCategories.addEventListener("change", () => {
+  categoriesListField.style.display = "flex";
+  categoriesListField.style.opacity = 1;
 
-	categoriesListField.style.display = 'flex'
-	categoriesListField.style.opacity = 1
-
-	if(fromCategories.checked == false){
-		categoriesListField.style.display = 'none'
-		categoriesListField.style.opacity = 0
-		wrapper.style.marginTop ='0px'
-	}
-})
+  if (fromCategories.checked == false) {
+    categoriesListField.style.display = "none";
+    categoriesListField.style.opacity = 0;
+    wrapper.style.marginTop = "0px";
+  }
+});
 
 let render = (data) => {
-	
-	data.forEach(el => {
-		
-		let created = new Date(el.created_at)
-		let now = new Date()
-		let milisec = Math.abs(created - now)
-		let time = Math.floor(milisec / (1000 * 60 * 60))
+  data.forEach((el) => {
+    let created = new Date(el.created_at);
+    let now = new Date();
+    let milisec = Math.abs(created - now);
+    let time = Math.floor(milisec / (1000 * 60 * 60));
 
-		let jokeBlock = document.createElement('div')
-		jokeBlock.classList.add('joke-block')
-		jokeBlock.dataset.id = el.id
+    let jokeBlock = document.createElement("div");
+    jokeBlock.classList.add("joke-block");
+    jokeBlock.dataset.id = el.id;
 
-		let like = document.createElement('div')
-		like.classList.add('like')
+    let like = document.createElement("div");
+    like.classList.add("like");
 
-		let likeImg = document.createElement('img')
-		likeImg.classList.add('like-img')
-		likeImg.setAttribute('src', 'visual/unliked.svg')
+    let likeImg = document.createElement("img");
+    likeImg.classList.add("like-img");
+    likeImg.setAttribute("src", "visual/unliked.svg");
 
-		let jokeWrapper = document.createElement('div')
-		jokeWrapper.classList.add('joke-wrapper')
+    let jokeWrapper = document.createElement("div");
+    jokeWrapper.classList.add("joke-wrapper");
 
-		let jokeLogo = document.createElement('div')
-		jokeLogo.classList.add('joke-logo')
+    let jokeLogo = document.createElement("div");
+    jokeLogo.classList.add("joke-logo");
 
-		let jokeLogoImg = document.createElement('img')
-		jokeLogoImg.setAttribute('src', 'visual/joke-img.svg')
+    let jokeLogoImg = document.createElement("img");
+    jokeLogoImg.setAttribute("src", "visual/joke-img.svg");
 
-		let joke = document.createElement('div')
-		joke.classList.add('joke')
+    let joke = document.createElement("div");
+    joke.classList.add("joke");
 
-		let jokeId = document.createElement('a')
-		jokeId.classList.add('joke-id')
-		jokeId.textContent= 'ID:'
-		
-		let jokeIdHref = document.createElement('a')
-		jokeIdHref.setAttribute('href', `https://api.chucknorris.io/jokes/${el.id}`)
-		jokeIdHref.textContent = `${el.id}`
+    let jokeId = document.createElement("a");
+    jokeId.classList.add("joke-id");
+    jokeId.textContent = "ID:";
 
-		let jokeText = document.createElement('p')
-		jokeText.classList.add('joke-text')
-		jokeText.textContent = el.value
+    let jokeIdHref = document.createElement("a");
+    jokeIdHref.setAttribute(
+      "href",
+      `https://api.chucknorris.io/jokes/${el.id}`
+    );
+    jokeIdHref.textContent = `${el.id}`;
 
-		let jokeInfo = document.createElement('div')
-		jokeInfo.classList.add('joke-info')
+    let jokeText = document.createElement("p");
+    jokeText.classList.add("joke-text");
+    jokeText.textContent = el.value;
 
-		let jokeDate = document.createElement('p')
-		jokeDate.classList.add('joke-date')
-		jokeDate.textContent = `Last update: ${time} hours ago`
+    let jokeInfo = document.createElement("div");
+    jokeInfo.classList.add("joke-info");
 
-		wrapper.prepend(jokeBlock)
+    let jokeDate = document.createElement("p");
+    jokeDate.classList.add("joke-date");
+    jokeDate.textContent = `Last update: ${time} hours ago`;
 
-		jokeBlock.appendChild(like)
-		jokeBlock.appendChild(jokeWrapper)
-		
-		like.appendChild(likeImg)
+    wrapper.prepend(jokeBlock);
 
-		if(el.liked){
-			favJokes.prepend(jokeBlock)
-			likeImg.src = 'visual/like.svg'
+    jokeBlock.appendChild(like);
+    jokeBlock.appendChild(jokeWrapper);
 
-			
-		jokeBlock.classList.add('joke-block-liked')
+    like.appendChild(likeImg);
 
-		like.classList.add('like-liked')
+    if (el.liked) {
+      favJokes.prepend(jokeBlock);
+      likeImg.src = "visual/like.svg";
 
-		likeImg.classList.add('like-img-liked')
+      jokeBlock.classList.add("joke-block-liked");
 
-		jokeWrapper.classList.add('joke-wrapper-liked')
+      like.classList.add("like-liked");
 
-		jokeLogo.classList.add('joke-logo-liked')
+      likeImg.classList.add("like-img-liked");
 
-		joke.classList.add('joke-liked')
+      jokeWrapper.classList.add("joke-wrapper-liked");
 
-		jokeId.classList.add('joke-id-liked')
+      jokeLogo.classList.add("joke-logo-liked");
 
-		jokeText.classList.add('joke-text-liked')
+      joke.classList.add("joke-liked");
 
-		jokeInfo.classList.add('joke-info-liked')
+      jokeId.classList.add("joke-id-liked");
 
-		jokeDate.classList.add('joke-date-liked')
+      jokeText.classList.add("joke-text-liked");
 
-		} else {
-			wrapper.prepend(jokeBlock)
-		}
+      jokeInfo.classList.add("joke-info-liked");
 
-		like.addEventListener('click', () => clickHeart(el))
+      jokeDate.classList.add("joke-date-liked");
+    } else {
+      wrapper.prepend(jokeBlock);
+    }
 
-		jokeWrapper.appendChild(jokeLogo)
-		jokeWrapper.appendChild(joke)
+    like.addEventListener("click", () => clickHeart(el));
 
-		jokeLogo.appendChild(jokeLogoImg)
+    jokeWrapper.appendChild(jokeLogo);
+    jokeWrapper.appendChild(joke);
 
-		joke.appendChild(jokeId)
+    jokeLogo.appendChild(jokeLogoImg);
 
-		jokeId.appendChild(jokeIdHref)
-		joke.appendChild(jokeText)
-		joke.appendChild(jokeInfo)
+    joke.appendChild(jokeId);
 
-		jokeInfo.appendChild(jokeDate)
-	})
+    jokeId.appendChild(jokeIdHref);
+    joke.appendChild(jokeText);
+    joke.appendChild(jokeInfo);
 
-}
+    jokeInfo.appendChild(jokeDate);
 
-let getRandomJoke = () => 
-    fetch('https://api.chucknorris.io/jokes/random')
+    if (el.categories.length !== 0) {
+      let jokeCategory = document.createElement("div");
+      jokeCategory.classList.add("joke-category");
 
-let getJokeByCategory = (category) => 
-    fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+      let jokeCategoryText = document.createElement("p");
+      jokeCategoryText.classList.add("joke-category-text");
+      jokeCategoryText.textContent = el.categories.toString().toUpperCase();
 
-let getFreeSpeach = (word) => 
-	fetch(`https://api.chucknorris.io/jokes/search?query=${word}`)
+      jokeInfo.appendChild(jokeCategory);
+      jokeCategory.appendChild(jokeCategoryText);
+    }
+  });
+};
 
+let getRandomJoke = () => fetch("https://api.chucknorris.io/jokes/random");
 
-let form = document.querySelector('form')
-let input = document.querySelector('input[type="text"]')
-let button = document.querySelector('button')
+let getJokeByCategory = (category) =>
+  fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
 
-form.addEventListener('submit', (event) => {
-	
-	event.preventDefault()
-	let radio = document.querySelector('.categories:checked')
-	if(radio){
-		getJokeByCategory(radio.value)
-		.then((res => res.json()))
-		.then((res => render([res])))
+let getFreeSpeach = (word) =>
+  fetch(`https://api.chucknorris.io/jokes/search?query=${word}`);
 
-	} else if (randomJoke.checked == true){
-		getRandomJoke()
-		.then((res => res.json()))
-		.then((res => render([res])))
-	}
-	
-	getFreeSpeach(input.value)
-	.then(res => res.json())
-	.then(res => render(res.result))
+let form = document.querySelector("form");
+let input = document.querySelector('input[type="text"]');
+let button = document.querySelector("button");
 
-	input.value = ''
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let radio = document.querySelector(".categories:checked");
+  if (radio) {
+    getJokeByCategory(radio.value)
+      .then((res) => res.json())
+      .then((res) => render([res]));
+  } else if (randomJoke.checked == true) {
+    getRandomJoke()
+      .then((res) => res.json())
+      .then((res) => render([res]));
+  } else if (input) {
+    getFreeSpeach(input.value)
+      .then((res) => res.json())
+      .then((res) => render(res.result));
+    input.value = "";
+  }
+
+  input.value = "";
+});
 let clickHeart = (data) => {
-	let img = document.querySelector(`.joke-block[data-id="${data.id}"] .like-img`)
-	let store = getStore()
-	if(img.src.includes('unliked')){
-		img.src = 'visual/like.svg'
-		setFavourite(data)
-
-	} else {
-		img.src = 'visual/unliked.svg'
-		removeFavourite(data, store)
-	}
-}
+  let img = document.querySelector(
+    `.joke-block[data-id="${data.id}"] .like-img`
+  );
+  let store = getStore();
+  if (img.src.includes("unliked")) {
+    img.src = "visual/like.svg";
+    setFavourite(data);
+  } else {
+    img.src = "visual/unliked.svg";
+    removeFavourite(data, store);
+  }
+};
 
 let setFavourite = (data) => {
-	let store = getStore()
-	if(store.findIndex(el => el.id === data.id) < 0) {
-		store.push({...data, liked: true})
-		localStorage.setItem('favourite', JSON.stringify(store))
-	    render([{...data, liked: true}])
-	}
-}
+  let store = getStore();
+  if (store.findIndex((el) => el.id === data.id) < 0) {
+    store.push({ ...data, liked: true });
+    localStorage.setItem("favourite", JSON.stringify(store));
+    render([{ ...data, liked: true }]);
+  }
+};
 
-let getStore = () => JSON.parse(localStorage.getItem('favourite')) ?? []
+let getStore = () => JSON.parse(localStorage.getItem("favourite")) ?? [];
 
 let removeFavourite = (data, store) => {
-	let updatedStore = store.filter(el => el.id !== data.id)
-	localStorage.setItem('favourite', JSON.stringify(updatedStore))
+  let updatedStore = store.filter((el) => el.id !== data.id);
+  localStorage.setItem("favourite", JSON.stringify(updatedStore));
 
-	favJokes.querySelector(`.joke-block[data-id="${data.id}"]`).remove()
+  favJokes.querySelector(`.joke-block[data-id="${data.id}"]`).remove();
 
-	let img = document.querySelector(`.joke-block[data-id="${data.id}"] .like-img`)
-	img.src = 'visual/unliked.svg'
-}
+  let img = document.querySelector(
+    `.joke-block[data-id="${data.id}"] .like-img`
+  );
+  img.src = "visual/unliked.svg";
+};
 
 let renderFavourite = () => {
-	let store = getStore()
-	render(store)
-	console.log(store)
-}
+  let store = getStore();
+  render(store);
+  console.log(store);
+};
 
-renderFavourite()
+renderFavourite();
